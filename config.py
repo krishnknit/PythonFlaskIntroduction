25094b8886d710e71edea58b189992592e33a758
krishnknit <krishnknit@gmail.com>
2017-03-26 00:20:01 +0530

    First commit for Python Intro

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# default config
+class BaseConfig(object):
+	DEBUG = False
+	SECRET_KEY = '\xfb\xa5\xc0\xe3\xf4\xc2\xa8"z\x8d\x8d|:\xd5VPg7\x01\x13;\x97%\xbb'
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+	DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+	DEBUG = False
